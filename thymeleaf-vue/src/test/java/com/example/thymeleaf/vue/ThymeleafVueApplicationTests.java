fcf53c81debc91163b09ad246162dca2939bafa9
Олег <oleg5432101@rambler.ru>
2020-02-19 12:36:43 +0300

    initial commit

---
+++ thymeleaf-vue/src/test/java/com/example/thymeleaf/vue/ThymeleafVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf.vue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
